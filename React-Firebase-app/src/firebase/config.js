c35dc78299dcf5dd8c53e71cdf353da020fefa8b
Aaron John <root1@Aarons-MacBook-Pro.local>
2022-08-22 18:41:32 +0530

    update

---
+++ React-Firebase-app/src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyBhskfUaBuS43eFztpcev-I_4WpkmHyIWA",
+  authDomain: "ketomenuserver.firebaseapp.com",
+  projectId: "ketomenuserver",
+  storageBucket: "ketomenuserver.appspot.com",
+  messagingSenderId: "810718218884",
+  appId: "1:810718218884:web:86313a13a3c760c79d50d1",
+  measurementId: "G-4PYKNS7TYZ"
+};
+ 
